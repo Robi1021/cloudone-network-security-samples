48e00400cc9f27069039ab4ca83932b2c533df7b
harpermcd <83316793+CSaaS-Repo@users.noreply.github.com>
2021-11-11 14:09:18 -0600

    Update provider.tf
    Took out Harper's creds

--- demo_terraform/provider.tf
+++ demo_terraform/provider.tf
@@ -9,11 +9,11 @@ terraform {
   }
 }
 
-# Harper Creds
+# Azure Creds
 provider "azurerm" {
   features {}
-  subscription_id = "e0b488d0-e8ed-426d-bb22-5dcce490cca9"
-  client_id       = "bf4ca2db-dc1c-485a-8f54-994d3c36ac27"
-  client_secret   = "g927Q~N1.6N_t8FDKzQ99L8gvfBKGfYGcIiIB"
-  tenant_id       = "3e04753a-ae5b-42d4-a86d-d6f05460f9e4"
+  subscription_id = "sub_id"
+  client_id       = "client_id"
+  client_secret   = "client_secret"
+  tenant_id       = "tenant_id"
 }
