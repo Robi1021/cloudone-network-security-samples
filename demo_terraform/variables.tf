464f4b32d616f775bd333784847c6f7838bfb599
harpermcd <83316793+CSaaS-Repo@users.noreply.github.com>
2021-11-12 12:15:35 -0600

    Add files via upload

--- demo_terraform/variables.tf
+++ demo_terraform/variables.tf
@@ -18,3 +18,28 @@ variable "demoenv" {
   description = "Demo Enviroment"
   default     = "Demo"
 }
+
+variable "demoenv" {
+  description = "Demo Enviroment"
+  default     = "Demo"
+}
+
+variable "sub_id" {
+  description = "Subscription_ID"
+  default     = "e0b488d0-e8ed-426d-bb22-5dcce490cca9"
+}
+
+variable "client_id" {
+  description = "Client_ID"
+  default     = "bf4ca2db-dc1c-485a-8f54-994d3c36ac27"
+}
+
+variable "client_secret" {
+  description = "Client_Secret"
+  default     = "g927Q~N1.6N_t8FDKzQ99L8gvfBKGfYGcIiIB"
+}
+
+variable "tenant_id" {
+  description = "Tenant_ID"
+  default     = "3e04753a-ae5b-42d4-a86d-d6f05460f9e4"
+}
